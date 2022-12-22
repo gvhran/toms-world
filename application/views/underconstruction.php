21adddcb57a0dd5ac37fe6987bba20ca69295356
carlocano03 <carlocano03@gmail.com>
2022-12-22 10:02:04 +0800

    Under maintenance

---
+++ application/views/underconstruction.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicons -->
+    <link href="./assets/img/main-icon.png" rel="icon">
+    <title>Under Maintenance</title>
+
+    <style>
+    * {
+        padding: 0;
+        margin: 0;
+    }
+
+    html,
+    body {
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    }
+    #constructions h1 {
+        color: red;
+        font-size: 50px;
+    }
+    </style>
+</head>
+
+<body>
+    <div id="constructions">
+        <h1>oopps!</h1>
+        <h2>This page is under construction - coordinate with IT Team</h2>
+    </div>
+</body>
+
+</html>
